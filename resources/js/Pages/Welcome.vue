469c78e743e458c055762cfc20d91eb31fa9a220
Robbie Love <Robbie@ubiquilife.com>
2022-10-16 21:18:13 +1000

    update welcome page contact form

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -31,36 +31,36 @@ defineProps({
 
                 <h2 class="mt-6 text-white text-2xl">
                     <p>If you are interested in buying this domain, please fill out the form below.</p>
-
-                    <form action="/contact" method="POST" class="mt-6">
-                        <input type="hidden" name="csrf" value="{{ csrf_token() }}">
-                        <div class="flex flex-col">
-                            <label for="name" class="text-white">Name</label>
-                            <input type="text" name="name" id="name" class="mt-1 p-2 rounded-lg border border-gray-300">
-                        </div>
-
-                        <div class="flex flex-col mt-4">
-                            <label for="email" class="text-white">Email</label>
-                            <input type="email" name="email" id="email" class="mt-1 p-2 rounded-lg border border-gray-300">
-                        </div>
-
-                        <div class="flex flex-col mt-4">
-                            <label for="message" class="text-white">Message</label>
-                            <textarea name="message" id="message" cols="30" rows="10" class="mt-1 p-2 rounded-lg border border-gray-300"></textarea>
-                        </div>
-
-                        <div class="flex justify-end mt-4">
-                            <button type="submit" class="p-2 rounded-lg bg-green-500 text-white">Submit</button>
-                        </div>
-                    </form>
                 </h2>
+
+                <form action="/contact" method="POST" class="mt-6">
+                    <input type="hidden" name="csrf" value="{{ csrf_token() }}">
+                    <div class="flex flex-col">
+                        <label for="name" class="text-white">Name</label>
+                        <input type="text" name="name" id="name" class="mt-1 p-2 rounded-lg border border-gray-300">
+                    </div>
+
+                    <div class="flex flex-col mt-4">
+                        <label for="email" class="text-white">Email</label>
+                        <input type="email" name="email" id="email" class="mt-1 p-2 rounded-lg border border-gray-300">
+                    </div>
+
+                    <div class="flex flex-col mt-4">
+                        <label for="message" class="text-white">Message</label>
+                        <textarea name="message" id="message" cols="30" rows="3" class="mt-1 p-2 rounded-lg border border-gray-300"></textarea>
+                    </div>
+
+                    <div class="flex justify-end mt-4">
+                        <button type="submit" class="p-2 rounded-lg bg-violet-900 text-white">Submit</button>
+                    </div>
+                </form>
             </div>
 
             <div class="flex justify-center mt-4 p-5 sm:items-center sm:justify-between">
                 <div class="text-center text-sm text-white sm:text-left">
                     <div class="flex items-center">
                         <p class="ml-1 text-white">
-                            Or call📞:
+                            Or call☎️:
                         </p>
                     </div>
                     <div class="mt-4 text-center text-3xl text-white sm:text-right sm:ml-0">
